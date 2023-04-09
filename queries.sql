a339253ded14d271997592269db0e3b7a5fe6eb7
Ryan Sakurai <ryansakurai03@gmail.com>
2023-04-09 16:17:23 -0300

    fix sintax errors

--- queries.sql
+++ queries.sql
@@ -11,18 +11,18 @@ group by playlist_name, user_nickname;
 
 -- What's the most liked song by a certain artist?
 select *
-from song
-where artist_name = <artist_name>
+from song s1
+where s1.artist_name = <artist_name>
 and song_qt_likes = (
-	select max(song_qt_likes)
-	from song
-	where artist_name = <artist_name>
-)
+	select max(s2.song_qt_likes)
+	from song s2
+	where s2.artist_name = s1.artist_name
+);
 
 -- How many followers does a certain artist have?
 select artist_name, artist_qt_followers
 from artist
-where artist_name = <artist_name>
+where artist_name = <artist_name>;
 
 -- Which users like genre X but not genre Y?
 (
@@ -40,8 +40,8 @@ except (
 select *
 from song s1
 where s1.artist_name = <artist_name>
-and song_duration = (
-	select min(song_duration)
+and s1.song_duration = (
+	select min(s2.song_duration)
 	from song s2
 	where s2.artist_name = s1.artist_name
 );
@@ -51,7 +51,8 @@ select r.artist_name, r.release_title, r.release_type, sum(s.song_duration) as r
 from release r
 join release_has_song rhs on (rhs.release_artist_name, rhs.release_title, rhs.release_type) = (r.artist_name, r.release_title, r.release_type)
 join song s on (s.artist_name, s.song_title) = (rhs.song_artist_name, rhs.song_title)
-where (r.artist_name, r.release_title, r.release_type) = (<artist_name>, <release_title>, 'Album');
+where (r.artist_name, r.release_title, r.release_type) = (<artist_name>, <release_title>, 'Album')
+group by r.artist_name, r.release_title, r.release_type;
 
 -- What's the duration of a certain album? (using views)
 select artist_name, release_title, release_type, release_duration
@@ -62,9 +63,9 @@ where (artist_name, release_title, release_type) = (<artist_name>, <release_titl
 select follower_nickname, playlist_creator_nickname, count(*) as qt_playlists
 from user_follows_playlist
 group by follower_nickname, playlist_creator_nickname
-having qt_playlists > 1;
+having count(*) > 1;
 
 -- What's the average quantity of likes in songs by each artist
 select artist_name, avg(song_qt_likes) as average_likes
 from song
-group by artist_name
+group by artist_name;
