cbf2abc9302e19cbfbfb2e600a93cec8dc522149
Ryan Sakurai <ryansakurai03@gmail.com>
2023-04-08 22:23:23 -0300

    translation (3/5)

--- views.sql
+++ views.sql
@@ -1,22 +1,22 @@
-create view artista_genero(artista_nome, genero_nome) as
-    select distinct artista_nome, genero_nome
-    from genero;
+create view artist_genre(artist_name, genre_name) as
+    select distinct artist_name, genre_name
+    from genre;
 
-create view lancamento_genero(artista_nome, lancamento_titulo, lancamento_tipo, genero_nome) as
-    select distinct rel.lancamento_artista_nome, rel.lancamento_titulo, rel.lancamento_tipo, genero_nome
-    from lancamento_contem_musica rel
-    join genero g on (g.artista_nome, g.musica_titulo) = (rel.musica_artista_nome, rel.musica_titulo);
+create view release_genre(artist_name, release_title, release_type, genre_name) as
+    select distinct rhs.release_artist_name, rhs.release_title, rhs.release_type, g.genre_name
+    from release_has_song rhs
+    join genre g on (g.artist_name, g.song_title) = (rhs.song_artist_name, rhs.song_title);
 
-create view lancamento_completo(artista_nome, lancamento_titulo, lancamento_tipo, lancamento_copyright, lancamento_data_publicacao, lancamento_qt_musicas, lancamento_duracao) as
-    select lanc.artista_nome, lanc.lancamento_titulo, lanc.lancamento_tipo, lancamento_copyright, lancamento_data_publicacao, count(*), sum(musica_duracao)
-    from lancamento lanc
-    join lancamento_contem_musica rel on (rel.lancamento_artista_nome, rel.lancamento_titulo, rel.lancamento_tipo) = (lanc.artista_nome, lanc.lancamento_titulo, lanc.lancamento_tipo)
-    join musica mus on (rel.musica_artista_nome, rel.musica_titulo) = (mus.artista_nome, mus.musica_titulo)
-    group by lanc.artista_nome, lanc.lancamento_titulo, lanc.lancamento_tipo;
+create view release_extended(artist_name, release_title, release_type, release_date, release_qt_songs, release_duration) as
+    select r.artist_name, r.release_title, r.release_type, release_date, count(*), sum(song_duration)
+    from release r
+    join release_has_song rhs on (rhs.release_artist_name, rhs.release_title, rhs.release_type) = (r.artist_name, r.release_title, r.release_type)
+    join song s on (rhs.song_artist_name, rhs.song_title) = (s.artist_name, s.song_title)
+    group by r.artist_name, r.release_title, r.release_type;
 
-create view playlist_completo(usuario_apelido, playlist_nome, playlist_qt_musicas, playlist_duracao, playlist_qt_seguidores) as
-    select p.usuario_apelido, p.playlist_nome, count(*), sum(musica_duracao), p.playlist_qt_seguidores
+create view playlist_extended(user_nickname, playlist_name, playlist_qt_songs, playlist_duration, playlist_qt_followers) as
+    select p.user_nickname, p.playlist_name, count(*), sum(song_duration), p.playlist_qt_followers
     from playlist p
-    natural join playlist_contem_musica
-    natural join musica
-    group by p.usuario_apelido, p.playlist_nome;
+    natural join playlist_has_song
+    natural join song
+    group by p.user_nickname, p.playlist_name;
